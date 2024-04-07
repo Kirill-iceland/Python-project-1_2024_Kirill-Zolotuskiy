375b8ed3d5cf78a09b5deb0fb10db3cb159d9c5a
Kirill Zolotuskiy <kirill.iceland@gmail.com>
2024-04-07 13:44:40 +0000

    Add scale to window size

--- src/maze/Maze.py
+++ src/maze/Maze.py
@@ -23,18 +23,21 @@ class Maze:
                 self.grid[i].append(Cell(default_wall))
 
     def draw(self, screen: pygame.Surface):
-        pygame.draw.rect(screen, (100, 100, 100), (10, 10, 10 * self.width, 10 * self.height))
+        screen_x, screen_y = screen.get_size()
+        length = min(screen_y / (self.height + 2), screen_x / (self.width + 2))
+        pygame.draw.rect(screen, (100, 100, 100), (length, length, length * self.width, length * self.height))
+        stroke = max(length / 10, 1)
         for i in range(self.height):
             for j in range(self.width):
                 if (self.grid[i][j].left_wall):
-                    pygame.draw.rect(screen, (255, 0, 0), (10 + (i * 10), 10 + (j * 10), 10, 1))
+                    pygame.draw.rect(screen, (255, 0, 0), (length + (i * length), length + (j * length), length, stroke))
 
                 if (self.grid[i][j].right_wall):
-                    pygame.draw.rect(screen, (255, 0, 0), (10 + (i * 10), 19 + (j * 10), 10, 1))
+                    pygame.draw.rect(screen, (255, 0, 0), (length + (i * length), 2 * length - stroke + (j * length), length, stroke))
 
                 if (self.grid[i][j].top_wall):
-                    pygame.draw.rect(screen, (255, 0, 0), (10 + (i * 10), 10 + (j * 10), 1, 10))
+                    pygame.draw.rect(screen, (255, 0, 0), (length + (i * length), length + (j * length), stroke, length))
 
                 if (self.grid[i][j].bottom_wall):
-                    pygame.draw.rect(screen, (255, 0, 0), (19 + (i * 10), 10 + (j * 10), 1, 10))
+                    pygame.draw.rect(screen, (255, 0, 0), (2 * length - stroke + (i * length), length + (j * length), stroke, length))
         return 0
