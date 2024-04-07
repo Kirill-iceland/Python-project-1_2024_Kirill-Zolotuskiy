79e268697ded96a4899b9f830f08f1708521a2b8
Kirill Zolotuskiy <kirill.iceland@gmail.com>
2024-04-07 13:44:57 +0000

    Add icon and caption

--- MazeCreator.py
+++ MazeCreator.py
@@ -6,12 +6,19 @@ from SpanningTree import SpanningTree
 
 pygame.init()
 
-screen = pygame.display.set_mode([500, 500])
-pygame.draw.rect(screen, (250, 100, 100), (100, 10, 100, 10))
-maze = SpanningTree.generate(5, 5)
+screen = pygame.display.set_mode([1000, 1000])
+icon = pygame.image.load('src/img/icon.png')
+pygame.display.set_icon(icon)
+pygame.display.set_caption('MazeCreator')
+# pygame.draw.rect(screen, (250, 100, 100), (100, 10, 100, 10))
+maze = SpanningTree.generate(100, 100)
 maze.draw(screen)
 pygame.display.flip()
-while (True):
-    1 + 1
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+pygame.quit()
 
 print(1)
