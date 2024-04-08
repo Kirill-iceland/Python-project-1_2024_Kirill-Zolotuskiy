0ce55cd6f50f6d174c12647ed059386d47b3b2ac
Kirill Zolotuskiy linux laptop <Kirill.Iceland@gmail.com>
2024-04-08 10:15:04 +0300

    Connect path nodes together

--- src/maze/Path.py
+++ src/maze/Path.py
@@ -3,10 +3,7 @@ from Maze import Maze
 
 
 class Path:
-    maze: Maze
-    path: list[tuple[int, int]] = []
-
-    def __init__(self, maze):
+    def __init__(self, maze: Maze):
         self.path = []
         self.maze = maze
 
@@ -17,9 +14,25 @@ class Path:
         screen_x, screen_y = screen.get_size()
         length = min(size_x / self.maze.height,
                      size_y / self.maze.width)
-        stroke = max(length / 5, 1)
+        stroke = max(length / 3, 1)
+        last_x = 0
+        last_y = 0
         for (cell_x, cell_y) in self.path:
-            pygame.draw.rect(screen, (50, 50, 255), (pos_x + stroke + (cell_x * length), pos_y +
-                             stroke + (cell_y * length), length - (2 * stroke), length - (2 * stroke)))
-            pygame.draw.rect(screen, (150, 150, 150), (pos_x + stroke + (cell_x * length), pos_y +
-                             stroke + (cell_y * length), length - (2 * stroke), length - (2 * stroke)), int(stroke) // 2)
+            if (last_x < cell_x):
+                pygame.draw.rect(screen, (50, 50, 255), (pos_x + stroke + (last_x * length), pos_y +
+                                 stroke + (last_y * length), 2 * length - (2 * stroke), length - (2 * stroke)))
+            elif (last_x > cell_x):
+                pygame.draw.rect(screen, (50, 50, 255), (pos_x + stroke + (cell_x * length), pos_y +
+                                 stroke + (cell_y * length), 2 * length - (2 * stroke), length - (2 * stroke)))
+            elif (last_y < cell_y):
+                pygame.draw.rect(screen, (50, 50, 255), (pos_x + stroke + (last_x * length), pos_y +
+                                 stroke + (last_y * length), length - (2 * stroke), 2 * length - (2 * stroke)))
+            elif (last_y > cell_y):
+                pygame.draw.rect(screen, (50, 50, 255), (pos_x + stroke + (cell_x * length), pos_y +
+                                 stroke + (cell_y * length), length - (2 * stroke), 2 * length - (2 * stroke)))
+            last_x = cell_x
+            last_y = cell_y
+            # pygame.draw.rect(screen, (50, 50, 255), (pos_x + stroke + (cell_x * length), pos_y +
+            #                  stroke + (cell_y * length), length - (2 * stroke), length - (2 * stroke)))
+            # pygame.draw.rect(screen, (150, 150, 150), (pos_x + stroke + (cell_x * length), pos_y +
+            #                  stroke + (cell_y * length), length - (2 * stroke), length - (2 * stroke)), int(stroke) // 2)
