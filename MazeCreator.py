3d2fd08a8dbf4253df06baca88abc627d33ff181
Kirill Zolotuskiy <kirill.iceland@gmail.com>
2024-04-12 15:30:44 +0000

    Add seperate class

--- MazeCreator.py
+++ MazeCreator.py
@@ -7,38 +7,44 @@ from src.windows.Button import Button
 from src.windows.Generate import Generate
 from src.windows.Solve import Solve
 
-pygame.init()
-pygame.font.init()
 
-screen = pygame.display.set_mode([1000, 1000])
-icon = pygame.image.load('src/img/icon.png')
-pygame.display.set_icon(icon)
-pygame.display.set_caption('MazeCreator')
-generate = Button(100, 550, 800, 200, 50)
-generate.set_text('GENERATE')
-generate.set_text_color((255, 255, 255))
-generate.set_color((150, 150, 150))
+class MazeCreator:
+    def start():
+        pygame.init()
+        pygame.font.init()
 
-solve = Button(100, 250, 800, 200, 50)
-solve.set_text('SOLVE')
-solve.set_text_color((255, 255, 255))
-solve.set_color((150, 150, 150))
+        screen = pygame.display.set_mode([1000, 1000])
+        icon = pygame.image.load('src/img/icon.png')
+        pygame.display.set_icon(icon)
+        pygame.display.set_caption('MazeCreator')
+        generate = Button(100, 550, 800, 200, 50)
+        generate.set_text('GENERATE')
+        generate.set_text_color((255, 255, 255))
+        generate.set_color((150, 150, 150))
 
-generate_window = Generate()
-solve_window = Solve()
+        solve = Button(100, 250, 800, 200, 50)
+        solve.set_text('SOLVE')
+        solve.set_text_color((255, 255, 255))
+        solve.set_color((150, 150, 150))
 
-running = True
-while running:
-    for event in pygame.event.get():
-        if generate.draw(screen):
-            running = generate_window.open(screen)
-            screen.fill((0, 0, 0))
-        if solve.draw(screen):
-            running = solve_window.open(screen)
-            screen.fill((0, 0, 0))
-        if solve.draw(screen):
-            running = False
-        if event.type == pygame.QUIT:
-            running = False
-    pygame.display.flip()
-pygame.quit()
+        generate_window = Generate()
+        solve_window = Solve()
+
+        running = True
+        while running:
+            for event in pygame.event.get():
+                if generate.draw(screen):
+                    running = generate_window.open(screen)
+                    screen.fill((0, 0, 0))
+                if solve.draw(screen):
+                    running = solve_window.open(screen)
+                    screen.fill((0, 0, 0))
+                if solve.draw(screen):
+                    running = False
+                if event.type == pygame.QUIT:
+                    running = False
+            pygame.display.flip()
+        pygame.quit()
+
+if __name__ == '__main__':
+    MazeCreator.start()
